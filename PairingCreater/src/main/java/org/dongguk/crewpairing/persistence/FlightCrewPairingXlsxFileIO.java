048f02022c05d48f6c477c4d5ed60fcc01828663
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2023-08-28 17:26:38 +0900

    feat: server catch용 log 추가

--- PairingCreater/src/main/java/org/dongguk/crewpairing/persistence/FlightCrewPairingXlsxFileIO.java
+++ PairingCreater/src/main/java/org/dongguk/crewpairing/persistence/FlightCrewPairingXlsxFileIO.java
@@ -1,14 +1,12 @@
 package org.dongguk.crewpairing.persistence;
 
 import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
 import org.dongguk.common.persistence.AbstractXlsxSolutionFileIO;
 import org.dongguk.crewpairing.app.PairingApp;
 import org.dongguk.crewpairing.domain.*;
-import org.drools.io.ClassPathResource;
 import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 
 import java.io.*;
@@ -28,10 +26,20 @@ public class FlightCrewPairingXlsxFileIO extends AbstractXlsxSolutionFileIO<Pair
     }
 
     @Override
-    public PairingSolution read(File inputFile) {
-        try (InputStream in = new ClassPathResource("input/ASCP_Data_Input_new.xlsx").getInputStream()) {
+    public PairingSolution read(File informationXlsxFile) {
+        try (InputStream in = new BufferedInputStream(new FileInputStream(informationXlsxFile))) {
             XSSFWorkbook workbook = new XSSFWorkbook(in);
             return new FlightCrewPairingXlsxReader(workbook).read();
+        } catch (IOException | RuntimeException e) {
+            log.error("{} {}", e.getMessage(), "Input File Error. Please Input File Format");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Pairing> readPairingList(List<Flight> flightList, File pairingXlsxFile) {
+        try (InputStream in = new BufferedInputStream(new FileInputStream(pairingXlsxFile))) {
+            XSSFWorkbook workbook = new XSSFWorkbook(in);
+            return new FlightCrewPairingXlsxReader(workbook).readPairingSet(flightList);
         } catch (IOException | RuntimeException e) {
             log.error("{} {}", e.getMessage(), "Input File Error. Please Input File Format");
             e.printStackTrace();
@@ -45,7 +53,6 @@ public class FlightCrewPairingXlsxFileIO extends AbstractXlsxSolutionFileIO<Pair
     }
 
     @Getter
-    @Setter
     private static class FlightCrewPairingXlsxReader extends AbstractXlsxReader<PairingSolution, HardSoftScore> {
         private final List<Aircraft> aircraftList = new ArrayList<>();
         private final List<Airport> airportList = new ArrayList<>();
@@ -61,15 +68,49 @@ public class FlightCrewPairingXlsxFileIO extends AbstractXlsxSolutionFileIO<Pair
         @Override
         public PairingSolution read() {
             readTimeData();
+            log.debug("Complete Read Time Data");
             readAircraft();         // 수정
+            log.debug("Complete Read Aircraft Data");
             readAirport();          // 수정
+            log.debug("Complete Read Airport Data");
             readDeadhead();         // 수정
+            log.debug("Complete Read Deadhead Data");
             readFlight();
+            log.debug("Complete Read Flight Data");
             return PairingSolution.builder()
                     .aircraftList(aircraftList)
                     .airportList(airportList)
                     .flightList(flightList)
                     .pairingList(createEntities()).build();
+        }
+
+        public List<Pairing> readPairingSet(List<Flight> inputFlightList) {
+            List<Pairing> list = new ArrayList<>();
+
+            nextSheet("Data");    // Sheet 고르기
+            currentRowIterator.next();              // 주제목 스킵
+
+            while (currentRowIterator.hasNext()) {
+                XSSFRow row = (XSSFRow) currentRowIterator.next();
+
+                Iterator<Cell> currentCellIterator =  row.cellIterator();
+
+                int indexCnt = (int) currentCellIterator.next().getNumericCellValue();
+                List<Flight> pair = new ArrayList<>();
+                while (currentCellIterator.hasNext()) {
+                    Cell cell = currentCellIterator.next();
+                    if (cell.getCellType() == CellType.BLANK || cell.getCellType() == CellType.STRING || cell.getNumericCellValue() == 0) {
+                        break;
+                    }
+
+                    pair.add(inputFlightList.get((int) cell.getNumericCellValue()));
+                }
+
+                list.add(new Pairing(indexCnt, pair, 0));
+            }
+
+            log.debug("Complete Read Pairing Data");
+            return list;
         }
 
         private void readTimeData() {
@@ -85,7 +126,8 @@ public class FlightCrewPairingXlsxFileIO extends AbstractXlsxSolutionFileIO<Pair
                     (int) row.getCell(2).getNumericCellValue(),
                     (int) row.getCell(3).getNumericCellValue(),
                     (int) row.getCell(4).getNumericCellValue(),
-                    (int) row.getCell(5).getNumericCellValue());
+                    (int) row.getCell(5).getNumericCellValue(),
+                    (int) row.getCell(6).getNumericCellValue());
 
             log.info("Complete Read Time Data");
         }
@@ -103,7 +145,6 @@ public class FlightCrewPairingXlsxFileIO extends AbstractXlsxSolutionFileIO<Pair
         private void readAircraft() {
             aircraftList.clear();
             nextSheet("Program_Cost");    // Sheet 고르기
-            currentRowIterator.next();              // 빈 행 스킵
             currentRowIterator.next();              // 주제목 스킵
             currentRowIterator.next();              // Header 스킵
 
@@ -251,28 +292,50 @@ public class FlightCrewPairingXlsxFileIO extends AbstractXlsxSolutionFileIO<Pair
             public void write() {
                 String timeStr = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss"));
                 exportPairingData(timeStr);
-                exportVisualData(timeStr);
+//                exportVisualData(timeStr);
+//                exportUserData(timeStr);
             }
 
             private void exportPairingData(String timeStr) {
-                String fileName = timeStr + "-pairingData.csv";
+                String fileName = timeStr + "-pairingData.xlsx";
 
-                List<Pairing> pairingList = solution.getPairingList();
-                StringBuilder text = new StringBuilder();
+                try (XSSFWorkbook workbook = new XSSFWorkbook()) {
+                    XSSFSheet sheet = workbook.createSheet("Data");
 
-                for (Pairing pairing : pairingList) {
-                    text.append(pairingList.indexOf(pairing)).append(",");
-                    for (Flight flight : pairing.getPair()) {
-                        text.append(flight.getIndex()).append(",");
+                    List<Pairing> pairingList = solution.getPairingList();
+
+                    //Pairing index 셀 스타일(우측 테두리)
+                    CellStyle rightBorder = workbook.createCellStyle();
+                    rightBorder.setAlignment(HorizontalAlignment.CENTER);
+                    rightBorder.setBorderRight(BorderStyle.THIN);
+
+                    Row row = sheet.createRow(0);
+                    Cell cell = row.createCell(0);
+                    cell.setCellValue("Pairing Data");
+
+                    //Pairing data 테이블
+                    int rowIdx = 1;
+                    for(Pairing pairing : pairingList){
+                        row = sheet.createRow(rowIdx);
+                        cell = row.createCell(0);
+                        cell.setCellValue(rowIdx-1);
+                        cell.setCellStyle(rightBorder);
+
+                        for(int i=0; i<pairing.getPair().size(); i++){
+                            cell = row.createCell(i+1);
+                            cell.setCellValue(pairing.getPair().get(i).getId());
+                        }
+                        rowIdx++;
                     }
-                    text.append("\n");
+
+                    try (FileOutputStream fo = new FileOutputStream("./data/crewpairing/output/" + fileName)) {
+                        workbook.write(fo);
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
                 }
 
-                try (FileWriter fw = new FileWriter("src/main/resources/output/" + fileName)) {
-                    fw.write(text.toString());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                System.out.println("Create Output File : " + fileName);
             }
 
             public void exportVisualData(String timeStr) {
@@ -357,6 +420,101 @@ public class FlightCrewPairingXlsxFileIO extends AbstractXlsxSolutionFileIO<Pair
             //분 단위를 버림함
             private static LocalDateTime stripMinutes(LocalDateTime l) {
                 return LocalDateTime.of(l.getYear(), l.getMonth(), l.getDayOfMonth(), l.getHour(), 0);
+            }
+
+            public void exportUserData(String timeStr) {
+                String fileName = timeStr + "-userData.xlsx";
+                try (XSSFWorkbook workbook = new XSSFWorkbook()) {
+                    XSSFSheet sheet = workbook.createSheet("Data");
+
+                    List<Pairing> pairingList = solution.getPairingList();
+                    LocalDateTime firstTime = pairingList.get(0).getPair().get(0).getOriginTime();
+                    LocalDateTime lastTime = firstTime;
+
+                    for (Pairing pairing : pairingList) {
+                        for (Flight flight : pairing.getPair()) {
+                            lastTime = lastTime.isAfter(flight.getDestTime()) ? lastTime : flight.getDestTime();
+                        }
+                    }
+
+                    //셀 스타일 모음
+                    CellStyle headerStyle = workbook.createCellStyle();
+                    Font headerFont = workbook.createFont();
+                    headerFont.setBold(true);
+                    headerStyle.setFont(headerFont);
+                    headerStyle.setBorderBottom(BorderStyle.DOUBLE);
+                    headerStyle.setFillForegroundColor(new XSSFColor(new byte[] {(byte) 226,(byte) 239,(byte) 217}, null));
+                    headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+                    headerStyle.setAlignment(HorizontalAlignment.CENTER);
+
+                    CellStyle contentStyle = workbook.createCellStyle();
+                    Font contentfont = workbook.createFont();
+                    contentfont.setFontHeightInPoints((short) 9);
+                    contentStyle.setAlignment(HorizontalAlignment.LEFT);
+                    contentStyle.setFont(contentfont);
+
+                    CellStyle rightBorder = workbook.createCellStyle();
+                    rightBorder.setBorderRight(BorderStyle.THIN);
+                    rightBorder.setAlignment(HorizontalAlignment.CENTER);
+
+                    //타임 테이블 헤더 작성
+                    Row row = sheet.createRow(0);
+                    Cell cell = row.createCell(0);
+                    cell.setCellValue(firstTime.format(DateTimeFormatter.ofPattern("yyyy")));
+                    cell.setCellStyle(headerStyle);
+
+                    int days = 0;
+                    for (LocalDateTime f = firstTime; ChronoUnit.DAYS.between(f, lastTime) >= 0; f = f.plusDays(1)) {
+                        days += 1;
+                        String MMdd = f.format(DateTimeFormatter.ofPattern("MM/dd"));
+                        cell = row.createCell(days);
+                        cell.setCellValue(MMdd);
+                        cell.setCellStyle(headerStyle);
+                    }
+
+                    //타임 테이블 내용 작성
+                    for(int i=0; i<pairingList.size(); i++){
+                        row = sheet.createRow(i+1);
+                        cell = row.createCell(0);
+                        cell.setCellValue("SET" + i);
+                        cell.setCellStyle(rightBorder);
+
+                        //Pairing의 flight에 대해서, 첫번째 비행과의 날짜 차이 k만큼 떨어진 셀에 내용 입력
+                        for(Flight flight : pairingList.get(i).getPair()){
+                            String sn = flight.getTailNumber();
+
+                            //도착 시간이 24시를 넘어가는 경우 (날짜 차이)*24 + 도착시간 으로 표시
+                            String origin = flight.getOriginTime().format(DateTimeFormatter.ofPattern("HH:mm"));
+                            int daysGap = (int) ChronoUnit.DAYS.between(flight.getOriginTime(), flight.getDestTime());
+                            int destHour = flight.getDestTime().getHour();
+                            int destMin = flight.getDestTime().getMinute();
+                            String dest = String.format("%02d:%02d", daysGap*24 + destHour, destMin);
+
+                            String text = "  ["+sn+"] " + "[ "+origin+" ~ "+dest+" ]";
+
+                            int k = (int) ChronoUnit.DAYS.between(firstTime, flight.getOriginTime())+1;
+
+                            //이미 셀에 값이 있다면 내용 추가
+                            if(row.getCell(k) == null){
+                                cell = row.createCell(k);
+                                cell.setCellValue(text);
+                            }
+                            else {
+                                StringBuilder sb = new StringBuilder(cell.getStringCellValue());
+                                cell.setCellValue(sb.append("  /").append(text).toString());
+                            }
+                            cell.setCellStyle(contentStyle);
+                            sheet.autoSizeColumn(k);
+                            sheet.setColumnWidth(k, sheet.getColumnWidth(k));
+                        }
+                    }
+
+                    try (FileOutputStream fo = new FileOutputStream("src/main/resources/output/" + fileName)) {
+                        workbook.write(fo);
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
             }
         }
     }
--- PairingCreater/src/main/java/org/dongguk/crewpairing/util/ViewAllConstraint.java
+++ PairingCreater/src/main/java/org/dongguk/crewpairing/util/ViewAllConstraint.java
@@ -1,5 +1,6 @@
 package org.dongguk.crewpairing.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.dongguk.crewpairing.domain.PairingSolution;
 import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 import org.optaplanner.core.api.score.buildin.hardsoftlong.HardSoftLongScore;
@@ -10,6 +11,7 @@ import java.util.Map;
 /**
  * solve 된 결과의 HardSoft score의 detail 확인하기 위한 클래스
  */
+@Slf4j
 public class ViewAllConstraint {
     /**
      * 각 제약 조건을 몇 번 어겼고, 얼마의 score를 만들어냈는지 확인
@@ -34,6 +36,8 @@ public class ViewAllConstraint {
             }
             System.out.println("Constraint: " + constraintName + "-> cost " + checkScore +" (violated "+ constraintMatchCount+" times)");
         }
-        System.out.println("total cost: "+(-solution.getScore().softScore()));
+
+        System.out.println("Hard Score : " + solution.getScore().hardScore());
+        System.out.println("Soft Score : " + solution.getScore().softScore());
     }
 }
