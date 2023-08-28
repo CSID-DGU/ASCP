4518b076ac01f66acfacff7229e947e6e1a0a8e1
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2023-08-28 17:56:07 +0900

    fix: 종료 조건 추가

--- ManagerProgram/src/main/java/org/dongguk/Main.java
+++ ManagerProgram/src/main/java/org/dongguk/Main.java
@@ -20,7 +20,8 @@ public class Main {
         List<Long> rlSoftScore = new ArrayList<>();
 
         // count만큼 사이클을 돌면서 Hard, Soft 점수를 저장
-        while(count-- > 0) {
+        for (int i = 0; i < count; i++) {
+            // Half Cycle: OptaPlanner 실행
             try {
                 ProcessBuilder pb = CommandUtil.getJavaCommand(inputFileName, pairingFileName);
 
@@ -48,6 +49,7 @@ public class Main {
                 e.printStackTrace();
             }
 
+//            // Half Cycle: Reinforcement Learning 실행
 //            try {
 //                ProcessBuilder pb = CommandUtil.getPythonCommand(inputFileName, pairingFileName);
 //
@@ -74,12 +76,22 @@ public class Main {
 //            } catch (Exception e) {
 //                e.printStackTrace();
 //            }
+//
+//            // 만약 사이클에서 변화가 없다면 종료(현재는 점수 변화가 없다면 끝나지만, 추후에는 다른 조건을 추가할 예정)
+//            if (otSoftScore.get(i) - rlSoftScore.get(i) == 0) {
+//                break;
+//            }
+
+            // 임시 종료 조건(OptaPlanner만 실행할 경우)
+            if (i != 0 && otSoftScore.get(i) - otSoftScore.get(i - 1) == 0) {
+                break;
+            }
         }
 
-        for (int j = 0; j < otHardScore.size(); j++) {
+        for (int i = 0; i < otHardScore.size(); i++) {
             // 인덱스와 같이 Hard, Soft 점수 출력
-            System.out.println("[ OT - " + j + " ] " + "Hard Score : " + otHardScore.get(j) + ", Soft Score : " + otSoftScore.get(j));
-//            System.out.println("[ RL - " + j + " ] " + "Hard Score : " + rlHardScore.get(j) + ", Soft Score : " + rlSoftScore.get(j));
+            System.out.println("[ OT - " + i + " ] " + "Hard Score : " + otHardScore.get(i) + ", Soft Score : " + otSoftScore.get(i));
+//            System.out.println("[ RL - " + i + " ] " + "Hard Score : " + rlHardScore.get(i) + ", Soft Score : " + rlSoftScore.get(i));
         }
     }
 }
